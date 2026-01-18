fbff385ba2ecd8e862e179248fadeb18a7db1e48
sharmar09565 <sharmar09565@gmail.com>
2026-01-18 11:37:13 +0530

    track page added

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
  registerGrievance.addEventListener("click", ()=>{
     window.location.href = "registerGrievance.html";
  });
+
+ let trackGrievance = document.querySelector(".secondary");
+ trackGrievance.addEventListener("click", () =>{
+   window.location.href = "trackGrievance.html";
+ });
